3d1c5b93e1fb2cbb50fca03f1b05962e25122354
Shashank <bshashankreddy2209@gmail.com>
2022-01-19 19:30:03 +0530

    changed keras to tf

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask_cors import CORS
 from flask import Flask,request,jsonify
 import os
-import keras
+import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL.Image import open
-from keras.applications import vgg16
-from keras.layers import Dense
-from keras.preprocessing import image
+
 app=Flask(__name__)
 CORS(app)
 
@@ -16,12 +13,12 @@ def hello():
     if request.method == 'POST':
         uploaded_file = request.files
         print(uploaded_file['data'].save(uploaded_file['data'].filename))
-        vgg_model=keras.models.load_model("./vgg_model_brain.h5")
+        vgg_model=tf.keras.models.load_model("./vgg_model_brain.h5")
         arr=np.asarray(open(uploaded_file['data'].filename).resize((224,224)))
         brain_img=arr.astype(np.float32)
-        img_array=image.img_to_array(brain_img)
+        img_array=tf.keras.preprocessing.image.img_to_array(brain_img)
         img_list=[]
-        img_list.append(keras.applications.vgg16.preprocess_input(img_array))
+        img_list.append(tf.keras.applications.vgg16.preprocess_input(img_array))
         X_test=np.array(img_list)
         predict_x=vgg_model.predict(X_test) 
         # y_pred = vgg_model.predict_classes(X_test)
@@ -34,6 +31,6 @@ def hello():
 @app.route("/test")
 def test():
     return "Test successful"
-    
+
 if __name__=="__main__":
     app.run(debug=True)
